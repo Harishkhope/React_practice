4a944ccb4ebfd7a04dd1746e78c9cc991c3fc38b
harish khope <harishkhope89@gmail.com>
2023-04-16 14:12:25 +0530

    module 4 of useState

---
+++ 04useState.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {useState} from "react";
+
+function App() {
+   const [textColour,setTextColour]=useState("black");
+    return( <div className="App" > <button onClick={()=>{
+      setTextColour(textColour==="black" ? "red" : "black");
+    }}>show/hide</button> <h1 style={{color:textColour}}>show or hide this text</h1>
+    </div> 
+    );
+}
+
+export default App;
