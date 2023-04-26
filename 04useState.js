4702df91c2917a5d1388d1945e8310e7732d1bab
harish khope <harishkhope89@gmail.com>
2023-04-26 21:00:08 +0530

    updated readme

--- 04useState.js
+++ 04useState.js
@@ -3,8 +3,7 @@ import {useState} from "react";
 
 function App() {
    const [textColour,setTextColour]=useState("black");
-    return( <div className="App" > <button onClick={()=>{
-      setTextColour(textColour==="black" ? "red" : "black");
+    return( <div className="App" > <button onClick={()=>{setTextColour(textColour==="black" ? "red" : "black");
     }}>show/hide</button> <h1 style={{color:textColour}}>colour changing text</h1>
     </div> 
     );
